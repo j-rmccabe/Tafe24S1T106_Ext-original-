4e93d3d7b85cfe20fa62f9e13b4899fa389aec30
John-Ross McCabe <john-Ross.mccabe@student.tafesa.edu.au>
2024-04-01 19:23:40 +1030

    Initial loading of files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Calculator
+{
+	public sealed partial class MainPage : Page
+	{
+		// add comment here
+		public static MainPage mainPage { get; set; }
+
+		// add comment here
+		public MainPage()
+		{
+			InitializeComponent();
+
+			mainPage = this;
+
+			Window.Current.CoreWindow.CharacterReceived += keyPress;
+		}
+
+		// add comment here
+		private void pageLoaded(object sender, RoutedEventArgs e)
+		{
+			// window minimum size
+			ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(256, 384));
+
+			// enable title bar full customiztion
+			CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+			// title bar customization
+			ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+			titleBar.ButtonBackgroundColor = Windows.UI.Colors.Transparent;
+			titleBar.ButtonInactiveBackgroundColor = Windows.UI.Colors.Transparent;
+			titleBar.ButtonInactiveForegroundColor = Windows.UI.Colors.White;
+		}
+
+		// add comment here
+		private void handleClearButtonClick(object sender, RoutedEventArgs e)
+		{
+			Utils.Utils.handleClearButtonClick();
+		}
+
+		// add comment here
+		private void handleEqualButtonClick(object sender, RoutedEventArgs e)
+		{
+			Utils.Utils.handleEqualButtonClick();
+		}
+
+		// add comment here
+		private async void keyPress(CoreWindow sender, CharacterReceivedEventArgs args)
+		{
+			if ((args.KeyCode >= 48 && args.KeyCode <= 57) || (args.KeyCode >= 96 && args.KeyCode <= 105))
+			{
+				uint number;
+
+				if (args.KeyCode >= 48 && args.KeyCode <= 57)
+					number = args.KeyCode - 48;             // pressed key 0 ~ 9
+				else
+					number = args.KeyCode - 96;				// pressed  0 ~ 9 on number keypad
+
+				Utils.Utils.handleClick(number.ToString());
+			}
+			else if (args.KeyCode == 27)
+			{
+				Utils.Utils.handleClearButtonClick();
+			}
+			else if (args.KeyCode == 13)
+			{
+				Utils.Utils.handleEqualButtonClick();
+			}
+			else if (args.KeyCode == '/')
+			{
+				Utils.Utils.handleOperationClick("/");
+			}
+			else if (args.KeyCode == '*')
+			{
+				Utils.Utils.handleOperationClick("*");
+			}
+			else if (args.KeyCode == '+')
+			{
+				Utils.Utils.handleOperationClick("+");
+			}
+			else if (args.KeyCode == '-')
+			{
+				Utils.Utils.handleOperationClick("-");
+			}
+			else if (args.KeyCode == 8)
+			{
+				Utils.Utils.handleBackspace();
+			}
+		}
+	}
+}
---
+++ Utils/Utils.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Data;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace Calculator.Utils
+{
+	public static class Utils
+	{
+		// Creates a Datatable object then computes the text parameter on the Datatable rows then coverts to string called number.
+		// Converts number from string to double called numberDouble.
+		// Converts numberDouble to Integer and returns.
+		private static double executeExpression(string text)
+		{
+			string number = new DataTable().Compute(text, "").ToString();
+			double numberDouble = Convert.ToDouble(number);
+
+			return numberDouble;
+		}
+
+		// Returns integer value of input char parameter character after converting to a string.
+		private static bool isNumeric(char character)
+		{
+			return int.TryParse(character.ToString(), out _);
+		}
+
+		//
+		public static void handleClick(string number)
+		{
+			TextBlock primaryDisplay = MainPage.mainPage.primaryDisplay;
+			TextBlock secondaryDisplay = MainPage.mainPage.secondaryDisplay;
+			TextBlock errorDisplay = MainPage.mainPage.errorDisplay;
+
+			// the number cannot have more than 6 digits
+			if (primaryDisplay.Text.Length < 6)
+			{
+				if (!(primaryDisplay.Text.Length == 0 && number == "0"))
+				{
+					if (secondaryDisplay.Text.Length + primaryDisplay.Text.Length <= 50)
+					{
+						primaryDisplay.Text += number;
+					}
+					else
+					{
+						errorDisplay.Text = "The exp. can have until 50 digits";
+						FlyoutBase.ShowAttachedFlyout(primaryDisplay);
+					}
+				}
+			}
+			else
+			{
+				errorDisplay.Text = "The number can have until 6 digits";
+				FlyoutBase.ShowAttachedFlyout(primaryDisplay);
+			}
+		}
+
+		// Displays text and operation in either primary or secondary display depending on the length.
+		public static void handleOperationClick(string operation)
+		{
+			TextBlock primaryDisplay = MainPage.mainPage.primaryDisplay;
+			TextBlock secondaryDisplay = MainPage.mainPage.secondaryDisplay;
+
+			if (primaryDisplay.Text.Length + secondaryDisplay.Text.Length < 50)
+			{
+				if (secondaryDisplay.Text.Length != 0)
+				{
+					if (!isNumeric(secondaryDisplay.Text[secondaryDisplay.Text.Length - 1]) && primaryDisplay.Text.Length == 0)
+						secondaryDisplay.Text = secondaryDisplay.Text.Substring(0, secondaryDisplay.Text.Length - 1) + operation;
+					else
+						secondaryDisplay.Text += primaryDisplay.Text + operation;
+				}
+				else if (primaryDisplay.Text.Length != 0)
+				{
+					secondaryDisplay.Text += primaryDisplay.Text + operation;
+				}
+
+				primaryDisplay.Text = "";
+			}
+		}
+
+		// Clears Text in primary and secondary displays on click.
+		public static void handleClearButtonClick()
+		{
+			MainPage.mainPage.primaryDisplay.Text = "";
+			MainPage.mainPage.secondaryDisplay.Text = "";
+		}
+
+		// Displays result of entered equation in primary display or displays errot message.
+		public static void handleEqualButtonClick()
+		{
+			TextBlock primaryDisplay = MainPage.mainPage.primaryDisplay;
+			TextBlock secondaryDisplay = MainPage.mainPage.secondaryDisplay;
+			TextBlock errorDisplay = MainPage.mainPage.errorDisplay;
+
+			try
+			{
+				primaryDisplay.Text = executeExpression(secondaryDisplay.Text + primaryDisplay.Text ).ToString();  //
+
+				if (primaryDisplay.Text.Length > 6)
+					primaryDisplay.FontSize = 48;
+				secondaryDisplay.Text = "";
+			}
+			catch
+			{
+				errorDisplay.Text = "There's an error in the exp.";
+				FlyoutBase.ShowAttachedFlyout(primaryDisplay);
+			}
+		}
+
+		// Deletes 1 letter or number when backspace is pressed.
+		public static void handleBackspace()
+		{
+			TextBlock primaryDisplay = MainPage.mainPage.primaryDisplay;
+
+			if (primaryDisplay.Text.Length > 0)
+				primaryDisplay.Text = primaryDisplay.Text.Substring(0, primaryDisplay.Text.Length - 1);
+		}
+	}
+}
